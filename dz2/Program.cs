b0854abbfd617ef30dcd1431cd2fdce0810394a6
AlekseyP <dr.Starc@ya.ru>
2022-07-14 14:01:02 +0300

    zadacha10

--- dz2/Program.cs
+++ dz2/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 10: Напишите программу, которая принимает на 
+вход трёхзначное число и на выходе показывает вторую 
+цифру этого числа.
+*/
+void zadacha10(){
+    Console.Write("Введите трёхзначное число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    if (99 > number || number > 999)
+    {
+        Console.WriteLine("Введённое число не подходит!");
+        return;
+    }
+    number = number / 10 % 10;
+    Console.WriteLine(number);
+ }
+
+zadacha10();
