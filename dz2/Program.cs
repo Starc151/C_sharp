6517b99f0546b4031009204b4cfbe205bf5c6e7a
AlekseyP <dr.Starc@ya.ru>
2022-07-14 22:39:41 +0300

    zadacha13

--- dz2/Program.cs
+++ dz2/Program.cs
@@ -1,7 +1,6 @@
 ﻿/*Задача 10: Напишите программу, которая принимает на 
 вход трёхзначное число и на выходе показывает вторую 
-цифру этого числа.
-*/
+цифру этого числа.*/
 void zadacha10(){
     Console.Write("Введите трёхзначное число: ");
     int number = Convert.ToInt32(Console.ReadLine());
@@ -14,4 +13,23 @@ void zadacha10(){
     Console.WriteLine(number);
  }
 
-zadacha10();
+/*Задача 13: Напишите программу, которая с помощью 
+деления выводит третью цифру заданного числа или 
+сообщает, что третьей цифры нет.*/
+ void zadacha13(){
+    Console.Write("Введите число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    if (number / 100 == 0){
+        Console.WriteLine("Третьей цифры нет!");
+        return;
+    }
+    else{
+        for (int i = 10; number / i > 100;){
+            number = number / i;
+        }
+        Console.WriteLine(number % 10);
+    }
+ }
+
+// zadacha10();
+zadacha13();
