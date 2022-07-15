a88d3a0431e433d0c80338585631d9fcc7cf2fed
AlekseyP <dr.Starc@ya.ru>
2022-07-15 17:00:58 +0300

    dz2

--- dz2/Program.cs
+++ dz2/Program.cs
@@ -7,7 +7,6 @@ void zadacha10(){
     if (99 > number || number > 999)
     {
         Console.WriteLine("Введённое число не подходит!");
-        return;
     }
     number = number / 10 % 10;
     Console.WriteLine(number);
@@ -21,7 +20,6 @@ void zadacha10(){
     int number = Convert.ToInt32(Console.ReadLine());
     if (number / 100 == 0){
         Console.WriteLine("Третьей цифры нет!");
-        return;
     }
     else{
         for (int i = 10; number / i > 100;){
@@ -31,5 +29,42 @@ void zadacha10(){
     }
  }
 
+ /*Задача 15: Напишите программу, которая принимает на 
+вход цифру, обозначающую день недели, и проверяет, 
+является ли этот день выходным.*/
+void zadacha15(){
+    Console.Write("Введите номкр дня недели: ");
+    int dayWeek = Convert.ToInt32(Console.ReadLine());
+    if (dayWeek < 1 || dayWeek > 7){
+        Console.Write("В неделе 7 дней, попробуйте снова: ");
+    }
+    switch (dayWeek)
+    {
+    case 1:
+        Console.WriteLine("Понедельник, не выходной");
+        break;
+    case 2:
+        Console.WriteLine("Вторник, не выходной");
+        break;
+    case 3:
+        Console.WriteLine("Среда, не выходной");
+        break;
+    case 4:
+        Console.WriteLine("Четверг, не выходной");
+        break;
+    case 5:
+        Console.WriteLine("Пятница, не выходной");
+        break;
+    case 6:
+        Console.WriteLine("Суббота, выходной");
+        break;
+    case 7:
+        Console.WriteLine("Воскресенье, выходной");
+        break;
+    }
+    
+}
+
 // zadacha10();
-zadacha13();
+// zadacha13();
+// zadacha15();
